b35ca2b54000c395a7074d0b01ed7851596672a9
Student <student@epbyminw1969.minsk.epam.com>
2017-03-02 18:56:53 +0300

    initial

---
+++ jobs.groovy
@@ -0,0 +1,153 @@
+job('MNTLAB-ikhamiakou-main-build-job') {
+    
+    parameters {
+        activeChoiceReactiveParam('jobs') {
+            choiceType('CHECKBOX')
+            groovyScript {
+                script('return ["MNTLAB-ikhamiakou-child1-build-job", "MNTLAB-ikhamiakou-child2-build-job", "MNTLAB-ikhamiakou-child3-build-job", "MNTLAB-ikhamiakou-child4-build-job"]')
+            }
+            
+        }
+        gitParam('BRANCH_NAME'){
+            type('BRANCH')
+        }
+
+    }
+    
+    scm {
+        github('MNT-Lab/mntlab-dsl', '$BRANCH_NAME')
+    }
+    
+    steps {
+        downstreamParameterized {
+            trigger('$jobs') {
+                block {
+                    buildStepFailure('FAILURE')
+                    failure('FAILURE')
+                    unstable('UNSTABLE')
+                }
+                parameters {
+                    predefinedProp('BRANCH_NAME', '$BRANCH_NAME')
+                    
+                }
+        }
+       
+        
+    }
+    
+    
+}
+
+job('MNTLAB-ikhamiakou-child1-build-job') {
+    
+    parameters{
+        activeChoiceReactiveParam('jobs') {
+            choiceType('SINGLE_SELECT')
+            groovyScript {
+                script('["ikhamiakou", "master"]')
+            }
+        }
+    }
+    scm {
+        github('MNT-Lab/mntlab-dsl')
+    }
+
+    steps {
+        shell('wget https://raw.githubusercontent.com/MNT-Lab/mntlab-dsl/ikhamiakou/work.sh')
+        shell('chmod +x work.sh')
+        shell('./work.sh')
+                
+            
+        publishers {
+        archiveArtifacts('*')
+        }
+    }
+    
+    
+}
+
+job('MNTLAB-ikhamiakou-child2-build-job') {
+    
+    parameters{
+        activeChoiceReactiveParam('jobs') {
+            choiceType('SINGLE_SELECT')
+            groovyScript {
+                script('["ikhamiakou", "master"]')
+            }
+        }
+    }
+    scm {
+        github('MNT-Lab/mntlab-dsl')
+    }
+    
+    steps {
+        shell('wget https://raw.githubusercontent.com/MNT-Lab/mntlab-dsl/ikhamiakou/work.sh')
+        shell('chmod +x work.sh')
+        shell('./work.sh')
+           
+            
+        publishers {
+        archiveArtifacts('*')
+        }
+    }
+    
+    
+}
+
+job('MNTLAB-ikhamiakou-child3-build-job') {
+    
+    parameters{
+        activeChoiceReactiveParam('jobs') {
+            choiceType('SINGLE_SELECT')
+            groovyScript {
+                script('["ikhamiakou", "master"]')
+            }
+        }
+    }
+    scm {
+        github('MNT-Lab/mntlab-dsl')
+    }
+
+    steps {
+        shell('wget https://raw.githubusercontent.com/MNT-Lab/mntlab-dsl/ikhamiakou/work.sh')
+        shell('chmod +x work.sh')
+        shell('./work.sh')
+                
+            
+        publishers {
+        archiveArtifacts('*')
+        }
+    }
+    
+    
+}
+
+job('MNTLAB-ikhamiakou-child4-build-job') {
+    
+    parameters{
+        activeChoiceReactiveParam('jobs') {
+            choiceType('SINGLE_SELECT')
+            groovyScript {
+                script('["ikhamiakou", "master"]')
+            }
+        }
+    }
+    
+    scm {
+        github('MNT-Lab/mntlab-dsl')
+    }
+
+
+    steps {
+        shell('wget https://raw.githubusercontent.com/MNT-Lab/mntlab-dsl/ikhamiakou/work.sh')
+        shell('chmod +x work.sh')
+        shell('./work.sh')
+         
+ 
+        publishers {
+        archiveArtifacts('*')
+        }
+    }
+    
+}    
+}
